f68e8934d0709cb0e8c99c89cef0f917b49979f3
gustavomdss <gustavo.dourados@gmail.com>
2022-10-19 15:33:23 -0400

    19/10/22_15:33

---
+++ aula03/calculadora.js
@@ -0,0 +1,11 @@
+let soma = require("./modules/somar");
+let subtrair = require("./modules/subtrair");
+let multiplicar = require("./modules/multiplicar");
+let divisao = require("./modules/dividir");
+
+console.log(soma(10,34));
+console.log(subtrair(23,10));
+console.log(multiplicar(25,4));
+console.log(multiplicar(11,0));
+console.log(divisao(75,5));
+console.log(divisao(75,0));
---
+++ aula03/modules/somar.js
@@ -0,0 +1,6 @@
+    let somar = function (num1, num2){
+        return num1 + num2
+    };
+
+
+    module.exports = somar;

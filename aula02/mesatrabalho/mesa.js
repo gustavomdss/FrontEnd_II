f04e8e1644993251f7e7d52895ebaea5531edcd0
gustavomdss <gustavo.dourados@gmail.com>
2022-10-19 11:26:00 -0400

    19/10/22_11:25

--- aula02/mesatrabalho/mesa.js
+++ aula02/mesatrabalho/mesa.js
@@ -1,9 +1,11 @@
+//Função que gera os numeros aleatoriamente;
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Variáveis que guardam os pontos dos jogadores
 var maquina = 0;
 
 var jogador = 0;
@@ -11,11 +13,11 @@ var jogador = 0;
 
 do {
 
-    var aleatorio = getRandomIntInclusive(1, 3);
+    var aleatorio = getRandomIntInclusive(1, 3); 
 
     var escolhaMaquina = 0;
 
-    switch (aleatorio) {
+    switch (aleatorio) {              //Transforma os numeros aleatórios em strings;
 
         case 1:
             escolhaMaquina = "pedra";
@@ -31,16 +33,11 @@ do {
             break;
     }
 
+ let escolhaUsuario = prompt("Escolha pedra, papel ou tesoura"); // Capta a escolha do jogador;
+ alert(escolhaMaquina);                                          // Mostra a escolha da maquina;
 
 
-    let escolhaUsuario = prompt("Escolha pedra, papel ou tesoura");
-
-    alert(escolhaMaquina);
-
-
-
-
-    if (escolhaMaquina == "pedra" && escolhaUsuario == "pedra") {
+    if (escolhaMaquina == "pedra" && escolhaUsuario == "pedra") {   // Compara os resultados e soma os pontos nas respectivas variáveis
         ganhador;
     } else if (escolhaMaquina == "pedra" && escolhaUsuario == "papel") {
         jogador += 1;
@@ -61,12 +58,15 @@ do {
     } else {
         exit()
     }
-    alert("Pontos Maquina: " + maquina)
-    alert("Pontos jogador: " + jogador)
+
+alert("Pontos Maquina: " + maquina);             // Mostra os pontos da maquina
+alert("Pontos jogador: " + jogador);             // Mostra os pontos do jogador
 
 } while (jogador < 2 && maquina < 2);
 
-if(jogador == 2){
+// Compara quem Ganhou
+
+if(jogador == 2){ 
     alert(" Voce Ganhou!")
 }else{
     alert("A maquina ganhou!")
